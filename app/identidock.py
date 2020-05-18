bbad41d250cb838dffd32db0f926d2fe95d43053
zhd4nov <prctmlm@gmail.com>
2020-05-19 00:23:00 +0300

    ensure passing tests

--- app/identidock.py
+++ app/identidock.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
+import html
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port='6379', db=0)
@@ -12,7 +13,7 @@ default_name = 'Mister User'
 def mainpage():
   name = default_name
   if request.method == 'POST':
-    name = request.form['name']
+    name = html.escape(request.form['name'], quote=True)
   
   salted_name = salt + name
   name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
@@ -31,7 +32,7 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-
+  name = html.escape(name, quote=True)
   image = cache.get(name)
   if image is None:
     print('Cache miss', flush=True)
